1080f4ce3eb3b8da5a699081f126ddf37eabf1d3
R00cKey <gabrieletorre99@gmail.com>
2023-05-09 16:31:15 +0200

    Macro per Gain

---
+++ GainMacro.C
@@ -0,0 +1,32 @@
+void GainMacro(){
+
+	TFile *file=new TFile("outputGain.root", "READ");
+	TFile *outfile=new TFile("gain.root", "RECREATE");
+	file->cd();
+	outfile->cd();
+	gDirectory->cd("Hist_Channels_Charge");	
+	for(int k=0;k<16;k++){
+	file->cd();
+	gDirectory->cd("Hist_Channels_Charge");	
+	TString name = Form("Hist_Channel_%d",k);
+	TH1D *h=(TH1D*)((TH1D*)gDirectory->Get(name)->Clone(Form("Gain_Fit_Channel_%d",k)));
+	h->Draw();
+
+	//Fitting data
+	TF1 *fun=new TF1("FitFun1", "gaus(0)+gaus(3)", -1.e-11 ,5.);  //0:ampiezza 1:media 2:sigma +gaus(3) 3:ampiezza 4:media ...
+	fun->SetParameter(0, h->GetBinContent(h->GetMaximumBin())); //Prendi valore nel bin(del bin più pieno)
+	fun->SetParameter(1, h->GetBinCenter(h->GetMaximumBin())); //Prendi il centro del bin(del bin più pieno)
+	fun->SetParameter(2, h->GetStdDev());
+	fun->SetParameter(3, h->GetBinContent(h->GetMaximumBin())/1000);
+	fun->SetParameter(4, h->GetBinCenter(h->GetMaximumBin())+2e-12);
+	fun->SetParameter(5, 1e-12);
+	fun->SetParLimits(1, -5.e-12, 5.e-12);
+	fun->SetParLimits(2, -1.e-12, 1.e-12);
+	fun->SetParLimits(4, -1.e-12, 5.e-12);
+	fun->SetParLimits(5, -1.e-12, 1.e-12);
+	h->Fit(fun, "R"); //In alternativa a fun "FitFun1"
+	outfile->cd();
+	h->Write();
+	}
+	outfile->Close();
+}
