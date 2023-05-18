390022d9c099aacf278bd471851df4949a4d61b4
edo <edo@Ubuntu.myguest.virtualbox.org>
2023-05-18 12:42:06 +0200

    macro aggiornata

--- ConstVarMacro.C
+++ ConstVarMacro.C
@@ -1,5 +1,16 @@
-void ConstVarMacro(string file_in, string file_out){
+void ConstVarMacro(string file_in, string file_out, string file_fitdata){
   
+  
+  /*
+  infile.push_back("file2.root");
+  infile.push_back("file3.root");
+  infile.push_back("file4.root");
+  infile.push_back("file5.root");
+  
+  TFile *file;
+  
+  for(int iF=0;iF<infile.size();iF++){
+  */
   
 	TFile *file=new TFile(file_in.data(), "READ"); 
 	TFile *outfile=new TFile(file_out.data(), "RECREATE"); //DISCLAIMER: "RECREATE" WILL OVERWRITE THE CONTENT OF THE OUTUPUT FILE
@@ -18,18 +29,24 @@ void ConstVarMacro(string file_in, string file_out){
 		TString name_2 = Form("Hist_TOF_cfm_ch%d_cut", l);
 		hTOF_cfm[l-2]=(TH1D*)((TH1D*)gDirectory->Get(name_1)->Clone(Form("Hist_TOF_cfm_ch%d", l)));
 		hTOF_cfm_cut[l-2]=(TH1D*)((TH1D*)gDirectory->Get(name_2)->Clone(Form("Hist_TOF_cfm_ch%d_cut", l)));
-		
+		hTOF_cfm[l-2]->Rebin(2);
+		hTOF_cfm_cut[l-2]->Rebin(2);
 	
-		fit_TOF_cfm[l-2] = new TF1("fit_TOF","gaus(0)+gaus(3)+[6]",-0.5,0.5);
+		fit_TOF_cfm[l-2] = new TF1(Form("fit_TOF_ch%d",l),"gaus(0)+gaus(3)+[6]",-0.5,0.5);
 		cout <<l<<endl;
 		fit_TOF_cfm[l-2] ->SetParameter(0,hTOF_cfm[l-2]->GetBinContent(hTOF_cfm[l-2]->GetMaximumBin()));
+		fit_TOF_cfm[l-2] ->SetParLimits(0,0,hTOF_cfm[l-2]->GetBinContent(hTOF_cfm[l-2]->GetMaximumBin())*1.5); 
 		fit_TOF_cfm[l-2] ->SetParameter(1,hTOF_cfm[l-2]->GetBinCenter(hTOF_cfm[l-2]->GetMaximumBin()));
-		fit_TOF_cfm[l-2] ->SetParameter(2,hTOF_cfm[l-2]->GetStdDev());
+		fit_TOF_cfm[l-2] ->SetParLimits(1,-20e-9,20e-9); 
+		fit_TOF_cfm[l-2] ->SetParameter(2,1e-9);
 		fit_TOF_cfm[l-2] ->SetParLimits(2,0.5e-9,2.2e-9); 
-		fit_TOF_cfm[l-2] ->SetParameter(3,hTOF_cfm[l-2]->GetBinContent(hTOF_cfm[l-2]->GetMaximumBin()));
+		fit_TOF_cfm[l-2] ->SetParameter(3,hTOF_cfm[l-2]->GetBinContent(hTOF_cfm[l-2]->GetMaximumBin())*0.1);
+		fit_TOF_cfm[l-2] ->SetParLimits(3,0,hTOF_cfm[l-2]->GetBinContent(hTOF_cfm[l-2]->GetMaximumBin())*0.5); 
 		fit_TOF_cfm[l-2] ->SetParameter(4,hTOF_cfm[l-2]->GetBinCenter(hTOF_cfm[l-2]->GetMaximumBin()));
+		fit_TOF_cfm[l-2] ->SetParLimits(4,-20e-9,20e-9); 
 		fit_TOF_cfm[l-2] ->SetParameter(5,hTOF_cfm[l-2]->GetStdDev());
-		fit_TOF_cfm[l-2] ->SetParLimits(5,2e-9,4e-9); 
+		fit_TOF_cfm[l-2] ->SetParLimits(5,1e-9,20e-9);
+		fit_TOF_cfm[l-2] ->SetParLimits(6,0,10); 
 		fit_TOF_cfm[l-2] ->SetNpx(10000);
 		
 		hTOF_cfm[l-2]->Fit(fit_TOF_cfm[l-2],"R");
@@ -38,18 +55,24 @@ void ConstVarMacro(string file_in, string file_out){
  	
  	
 		
-		hTOF_cfm[l]->SetAxisRange(-0.05e-6,0.05e-6);
+		hTOF_cfm[l-2]->SetAxisRange(-0.05e-6,0.05e-6);
 		
 		
-			fit_TOF_cfm_cut[l-2] = new TF1("fit_TOF","gaus(0)+gaus(3)+[6]",-0.5,0.5);
-		fit_TOF_cfm[l-2] ->SetParameter(0,hTOF_cfm_cut[l-2]->GetBinContent(hTOF_cfm_cut[l-2]->GetMaximumBin()));
+		fit_TOF_cfm_cut[l-2] = new TF1(Form("fit_TOF_cut_ch%d",l),"gaus(0)+gaus(3)+[6]",-0.5,0.5);
+		fit_TOF_cfm_cut[l-2] ->SetParameter(0,hTOF_cfm_cut[l-2]->GetBinContent(hTOF_cfm_cut[l-2]->GetMaximumBin()));
+		fit_TOF_cfm_cut[l-2] ->SetParLimits(0,0,hTOF_cfm_cut[l-2]->GetBinContent(hTOF_cfm_cut[l-2]->GetMaximumBin())*1.5); 
 		fit_TOF_cfm_cut[l-2] ->SetParameter(1,hTOF_cfm_cut[l-2]->GetBinCenter(hTOF_cfm_cut[l-2]->GetMaximumBin()));
-		fit_TOF_cfm_cut[l-2] ->SetParameter(2,hTOF_cfm_cut[l-2]->GetStdDev());
+		fit_TOF_cfm_cut[l-2] ->SetParLimits(1,-20e-9,20e-9); 
+		fit_TOF_cfm_cut[l-2] ->SetParameter(2,1e-9);
 		fit_TOF_cfm_cut[l-2] ->SetParLimits(2,0.5e-9,2.2e-9); 
-		fit_TOF_cfm_cut[l-2] ->SetParameter(3,hTOF_cfm_cut[l-2]->GetBinContent(hTOF_cfm_cut[l-2]->GetMaximumBin()));
+		fit_TOF_cfm_cut[l-2] ->SetParameter(3,hTOF_cfm_cut[l-2]->GetBinContent(hTOF_cfm_cut[l-2]->GetMaximumBin())*0.1);
+		fit_TOF_cfm_cut[l-2] ->SetParLimits(3,0,hTOF_cfm_cut[l-2]->GetBinContent(hTOF_cfm_cut[l-2]->GetMaximumBin())*0.5);
 		fit_TOF_cfm_cut[l-2] ->SetParameter(4,hTOF_cfm_cut[l-2]->GetBinCenter(hTOF_cfm_cut[l-2]->GetMaximumBin()));
+		fit_TOF_cfm_cut[l-2] ->SetParLimits(4,-20e-9,20e-9); 
 		fit_TOF_cfm_cut[l-2] ->SetParameter(5,hTOF_cfm_cut[l-2]->GetStdDev());
-		fit_TOF_cfm_cut[l-2] ->SetParLimits(5,2e-9,4e-9); 
+		fit_TOF_cfm_cut[l-2] ->SetParLimits(5,1e-9,20e-9); 
+		fit_TOF_cfm_cut[l-2] ->SetParameter(6,0.5);
+		fit_TOF_cfm_cut[l-2] ->SetParLimits(6,0,10);
 		fit_TOF_cfm_cut[l-2]	->SetNpx(10000);
 		
 		hTOF_cfm_cut[l-2]->Fit(fit_TOF_cfm_cut[l-2],"R");
@@ -59,7 +82,7 @@ void ConstVarMacro(string file_in, string file_out){
 		
 		hTOF_cfm_cut[l-2]->SetAxisRange(-0.05e-6,0.05e-6);
 		
-		gDirectory->cd("..");
+		//gDirectory->cd("..");
 		
 		outfile->cd();
 		
@@ -68,9 +91,9 @@ void ConstVarMacro(string file_in, string file_out){
 		hTOF_cfm[l-2]->Write();
 		hTOF_cfm_cut[l-2]->Write();
 	
-		cout <<l<<endl;
 
 	}
-	
+	file->Close();
+	outfile->Close();
 	
 }
