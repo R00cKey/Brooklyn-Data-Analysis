483a7852fa44ce7b6bd7f42f91b006f0726c8452
R00cKey <gabrieletorre99@gmail.com>
2023-06-06 15:40:04 +0200

    Efficiency macro correction

--- EfficiencyMacro.C
+++ EfficiencyMacro.C
@@ -1,10 +1,14 @@
 void EfficiencyMacro(){
 
-	TFile *file=new TFile("output0.2.root", "READ");
+	TFile *file=new TFile("outputMuon_6.root", "READ");
 	ifstream infofile;
 	string fileline; //A line of the File
 	infofile.open("gain.dat");
 	getline(infofile,fileline);
+	file->cd();
+	gDirectory->cd("Hist_Total_Charge");	
+	TString name = Form("Hist_total_charge");
+	TH1D *h=(TH1D*)(gDirectory->Get(name));
 	for(int k=0;k<16;k++){
 		int nevent=0;
 		double ped_mean, ped_std;
@@ -20,7 +24,7 @@ void EfficiencyMacro(){
 		for(int i=1; i<=500;i++){
 			if(h->GetBinCenter(i)>ped_mean+3*ped_std){nevent+=h->GetBinContent(i);}
 		}
-	cout << k << "\t" << nevent << endl;
+	cout << k << "\t" << nevent/h->GetEntries() << endl;
 	}
 	infofile.close();
 }
